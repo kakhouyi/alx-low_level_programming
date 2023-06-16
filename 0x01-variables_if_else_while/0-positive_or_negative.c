727d1c042f3ea85e566232dbb8461e7f585c5161
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-16 13:27:00 -0700

    print alphabet

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -12,7 +13,7 @@ int main(void)
 
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*declare a variabe*/
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		/*print the value of the variabe*/
+
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
