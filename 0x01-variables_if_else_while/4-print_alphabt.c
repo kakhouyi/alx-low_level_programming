d99f4e1425c1f324dc85c03e8d48d10374d8f35a
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-16 14:50:24 -0700

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-	/* dummy comment */
+	/**
+	 * main - Entry point
+	 *
+	 * description: 'the program's description'
+	 * @parameter: describe the parameter
+	 *
+	 * Return: Always 0 (Success)
+	 */
 	char alphabet;
 
 	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
