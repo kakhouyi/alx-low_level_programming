0a820dd9cebf915c75579e310736c105c5764842
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-16 14:41:14 -0700

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,14 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main(void)
 {
-	/*declare a variabe*/
-
 	char alphabet;
-
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 	{
-		/*print the value of the variabe*/
-
 		if (alphabet != 'e' && alphabet != 'q')
 		{
 			putchar(alphabet);
 		}
 	}
-
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
