6fe62ee984e53931d396d65a3d99d72e48f13056
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-18 04:20:26 -0700

    size data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,18 +6,13 @@
  */
 main(void)
 {
+	printf("char is %2d bytes \n", sizeof(char));
 	printf("int is %2d bytes \n", sizeof(int));
 	printf("long int is %2d bytes \n", sizeof(long int));
-	printf("long int * is %2d bytes \n", sizeof(long int *));
+	printf("long long int is %2d bytes \n", sizeof(double));
 	printf("signed int is %2d bytes \n", sizeof(signed int));
 	printf("unsigned int is %2d bytes \n", sizeof(unsigned int));
-	printf("\n");
 	printf("float is %2d bytes \n", sizeof(float));
-	printf("double is %2d bytes \n", sizeof(double));
-	printf("long double is %2d bytes \n", sizeof(long double));
 	printf("\n");
-	printf("signed char is %2d bytes \n", sizeof(signed char));
-	printf("char is %2d bytes \n", sizeof(char));
-	printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
 	return (0);
 }
