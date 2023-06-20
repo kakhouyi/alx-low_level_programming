272e01010f488aa776d062ad7ce37d4d78d44608
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-20 12:37:06 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_last_digit- print sigh of number
+* @n: a number
+*
+* * Return: 0
+*/
+
+int print_last_digit(int n)
+{
+
+	int r;
+
+	r = n % 10;
+	if (n < 0)
+	{
+		r = r * -1;
+	}
+	return (0);
+}
