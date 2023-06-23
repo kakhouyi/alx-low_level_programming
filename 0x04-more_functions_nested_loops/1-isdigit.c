aafd0814b405d7b0aea5c3d5e0e6103ae8bfd87b
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-23 12:51:30 -0700

    isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - function shows the upper
+ * @c: is caracter
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+
+{
+
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
