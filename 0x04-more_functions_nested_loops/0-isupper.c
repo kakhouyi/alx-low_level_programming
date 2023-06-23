3e37b963b43be850f5918dd3803b104d60c9901e
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-23 12:18:29 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * @c: is caracter
+ * Return: Always 0.
+ */
+int _isupper(int c);
+{
+	char c;
+
+	for (c = 'a'; c >= 'z'; c++)
+	if(l == c) 
+	{ 
+	return (0);
+	}
+	else
+	{
+	return (1)
+	}
+}
