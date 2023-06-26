ed829c79b52fdaf1d92f2d44003502f77dbd7285
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-26 11:47:00 -0700

    swap 2 integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: a number
+ * @b: a number
+ * Return:
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*b = c;
+}
