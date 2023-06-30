98f1adad99aaf7c0e86c75bac48ae3401f460656
kaoutar <kaoutar.khouyi40@gmail.com>
2023-06-30 06:36:05 -0700

    comparing

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,33 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	if (s1[i] < s2[i])
-	{
-	return (-15);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	else
-	if (s1[i] > s2[i])
-	{
-	return (15);
-	}
-
-	i++;
-	}
-
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
 	return (0);
-	}
-	else if (s1[i] == '\0')
-	{
-	return (-15);
-	}
-	else
-	{
-	return (15);
-	}
 }
